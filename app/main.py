4e141807deeba2d080ca8ec2ff6e24e011bb07e2
carter <carter@carter.lan>
2020-04-25 13:51:46 +0800

    add api

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
-from flask import Flask 
-  
-app = Flask(__name__) 
-  
-@app.route("/") 
-def home_view(): 
-        return "<h1>Welcome to Geeks for Geeks</h1>"
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_view():
+    return "<h1>Welcome to Geeks for Geeks</h1>"
+
+
+@app.route("/test")
+def func():
+    return jsonify({"welcome": "world"})
