23d4492e1b37a5e99bcf2b42ca4387255d998298
carter <carter@carterwork.local>
2020-04-29 14:59:43 +0800

    delete useed function

--- packages/main.py
+++ packages/main.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -7,25 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-    return "<h1>Welcome to Geeks for Geeks</h1>"
+    return "<h1>Welcome to slack for slack</h1>"
 
 
 @app.route("/message", methods=['GET', 'POST'])
 def channel_message():
+    print("\n\n")
     print(request.json)
     return request.json
-
-
-@app.route("/test", methods=['GET', 'POST'])
-def func():
-    print(request.args)
-    print(request.json)
-    print(request.form)
-
-    return jsonify(
-        {
-            "args": request.args,
-            "json": request.json,
-            "form": request.form
-        }
-    )
