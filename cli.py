ffe8c4997979c558171aa1591598ec05a8cc65d6
carter.hong@zoom.us <carter.hong@zoom.us>
2020-05-26 14:24:34 +0800

    fix bug

--- cli.py
+++ cli.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 # !/usr/bin/env python
 # author = Chris Hong
+from dataclasses import dataclass
+
 import click
+import requests
 from sqlalchemy.ext.declarative import declarative_base
 
 from mantis import db
 
+from mantis.models import *
+
 
 def init_db():
-    from mantis.models.token import Token
-    from mantis.models.user import User
-    from mantis.models.slack import Slack
+    click.echo('Initialized the database')
+    # from mantis.models.token import Token
+    # from mantis.models.user import User
+    # from mantis.models.slack import Slack
+
     Base = declarative_base()
     db.create_all()
 
@@ -21,5 +28,38 @@ def init():
     init_db()
 
 
+@dataclass
+class SlackUser:
+    workspace: str
+    username: str
+    password: str
+    description: str
+    token: str
+    cookie: str
+
+
+account_level_enable_pmi = SlackUser(
+    workspace="carterbot",
+    username="carter.hong@zoomus.ltd",
+    password="Slack.123456",
+    description="Admin账号",
+    token="",
+    cookie=""
+)
+
+
+def init_user(user):
+    resp = requests.post("https://devslackbot.zoomdev.us/api/account/add", data={
+        "username": user.username,
+        "password": user.password,
+        "workspace": user.workspace,
+        "token": user.token,
+        "cookie": user.cookie
+    })
+
+    print(resp.text)
+
+
 if __name__ == '__main__':
-    init()
+    init_db()
+    init_user(account_level_enable_pmi)
