79a97c2f5e32b4144e508c2351a4e15716385e47
carter.hong@zoom.us <carter.hong@zoom.us>
2020-06-11 09:23:25 +0800

    fix bug

--- mantis/interceptor.py
+++ mantis/interceptor.py
@@ -121,7 +121,7 @@ def login_filter():
 @ignore_exception
 def show_request_param():
     if app.debug:
-        print("User:" + request.cookies.get(Auth.USERNAME, None) + ", Url：" + str(request.path))
+        print("User:" + request.cookies.get(Auth.USERNAME, "None User") + ", Url：" + str(request.path))
 
 
 @ignore_exception
